b89deb4c2e076c6a9bfa319dce0367c758422be6
unknown <shitijbhatnagar@yahoo.com>
2025-06-01 11:14:04 +0530

    Added logging to the service

--- src/main/java/com/sb/tower/web/TowerController.java
+++ src/main/java/com/sb/tower/web/TowerController.java
@@ -3,15 +3,33 @@ package com.sb.tower.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/tower")
 public class TowerController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TowerController.class);
+
     @GetMapping
     public String processEchoMessage() {
-        Logger.getLogger(TowerController.class.getName()).info("Tower Service is Up");
-        return "Tower Service is Up";
+        LOG.info("Tower Service is ready");
+        System.out.println("Tower Service is ready");
+        return "Tower Service is ready";
+    }
+
+    @GetMapping("/bootstrap")
+    public String processBootstrapMessage() {
+        LOG.info("Tower Service has bootstrapped");
+        System.out.println("Tower Service has bootstrapped");
+        return "Tower Service has bootstrapped";
+    }
+
+    @GetMapping("/submit")
+    public String processSubmitpMessage() {
+        LOG.info("Tower Service has submitted request");
+        System.out.println("Tower Service has submitted request");
+        return "Tower Service has submitted request";
     }
 }
